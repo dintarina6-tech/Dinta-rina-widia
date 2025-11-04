acf1d3f0c0c02675ec329d8f8b77f08b6a665e5e
dintarina6-tech <dintarina6@gmail.com>
2025-11-04 10:01:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+let nama = "Dinta rina widia";
+let kelas = "X pplg 2";
+let umurSaya = 16;
+let hobiSaya = "renang";
+let statusSaya = "pelajar";
+
+console.log(nama);
+console.log(kelas);  
+console.log(umurSaya);
+console.log(hobiSaya);
+console.log(statusSaya);
+
+// penjumlahan 
+let bilangan1 = 5
+let bilangan2 = 15
+console.log(bilangan1 + bilangan2)
+
+// pengurangan
+let $bilangan1 = 25
+let $bilangan2 = 15
+console.log($bilangan1 - $bilangan2)
+
+// perkalian
+let Bilangan1 = 10 
+let Bilangan2 = 3
+console.log(Bilangan1 ** Bilangan2)
+
+// pembagian 
+let bilangan_1 = 80
+let bilangan_2 = 4
+console.log(bilangan_1 / bilangan_2)
+
+// eksponen (pangkat)
+let eksponen1 = 6
+let eksponen2 = 3
+console.log(eksponen1 ** eksponen2)
+
+// modulus 
+let modulus1 = 25
+let modulus2 = 10
+console.log(modulus1 % modulus2)
+
+// increment
+let increment1 = 36
+increment1++ 
+console.log(increment1)
+
+//dicrement
+let dicrement_ = 10
+dicrement_--; 
+console.log(dicrement_)
+
+
+    
